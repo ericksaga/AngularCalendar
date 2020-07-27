6322be8797cb3405a5d207c094b123d3afbdc109
ericksaga <erickmedina09@gmail.com>
2020-07-26 22:05:30 -0400

    added form tag and groups to the login screen

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Router } from '@angular/router';
 import { DataManagementService } from 'src/app/services/dataManagement/data-management.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,22 +11,26 @@ import { DataManagementService } from 'src/app/services/dataManagement/data-mana
 })
 export class LoginComponent implements OnInit {
 
-  email: string;
-  password: string;
+  loginForm: FormGroup;
   constructor(private auth: AuthService,
               private router: Router,
-              private dataService: DataManagementService) { }
+              private dataService: DataManagementService,
+              private fb: FormBuilder) { }
 
   ngOnInit(): void {
     if (this.dataService.getUser()) {
       this.router.navigate(['calendar']);
       console.log('user Logged in');
     }
+    this.loginForm = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.minLength(4)]
+    });
   }
 
   login() {
     console.log('logging in');
-    this.auth.login(this.email, this.password).then((user) => {
+    this.auth.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value).then((user) => {
       this.dataService.setUser(user);
       this.router.navigate(['calendar']);
     },
