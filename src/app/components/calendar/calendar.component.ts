7fac9a92acd6666a916d5ddf31ff7b4771458db9
ericksaga <erickmedina09@gmail.com>
2020-07-26 21:18:14 -0400

    added buttons to move throught months

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { User } from 'src/app/classes/user';
 import { Day } from 'src/app/classes/day';
 import { CalendarSquare } from 'src/app/classes/calendar-square';
@@ -6,6 +6,7 @@ import { DatabaseService } from 'src/app/services/database/database.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Router, NavigationExtras } from '@angular/router';
 import { DataManagementService } from 'src/app/services/dataManagement/data-management.service';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-calendar',
@@ -29,58 +30,32 @@ export class CalendarComponent implements OnInit {
     {month: 'diciembre', days: 31},
   ];
   calendarSq: Array<Array<CalendarSquare>>;
+  dateOnScreen: Date;
+  endDay: number;
   monthResume: Day;
+  date: Subject<Date>;
+  dateSub: Subscription;
   monthCosts = 0;
+  calendarDate: string;
   constructor(private db: DatabaseService,
               private router: Router,
               private dataService: DataManagementService) { }
 
   ngOnInit(): void {
-    this.monthResume = new Day();
-    this.calendarSq = new Array<Array<CalendarSquare>>();
-    for (let x = 0; x < 7; x++) {
-      this.calendarSq.push(new Array<CalendarSquare>());
-    }
-    const date: Date = new Date();
-    const dateSt: Date = new Date(date.getFullYear(), date.getMonth());
-    let lmt: number = this.calendar[date.getMonth()].days;
-    let st = 1;
-    if (date.getMonth() === 1 && date.getFullYear() % 4 === 0) {
-      lmt++;
-    }
-    st -= dateSt.getDay();
-    console.log(st);
-    for (let x = 0; x < 42; x++) {
-      let sq = new CalendarSquare();
-      const p = Math.floor(x / 7);
-      sq.index = x + st;
-      if (x + st <= 0 || x + st > lmt) {
-        sq.active = -1;
-      } else {
-        sq.active = 0;
-      }
-      this.calendarSq[p].push(sq);
-    }
     if (!this.dataService.getUser()) {
       this.router.navigate(['']);
     } else {
-      this.db.getMonthInfo(date, lmt).subscribe((data: Array<Day>) => {
-        for (let sqArray of this.calendarSq) {
-          for (let sq of sqArray) {
-            const pos = data.findIndex((day) => day.created.getDate() === sq.index);
-            if ( pos !== -1) {
-              sq.active = 1;
-              this.monthResume.gains += data[pos].gains;
-              this.monthResume.type1Clothes += data[pos].type1Clothes;
-              this.monthResume.type2Clothes += data[pos].type2Clothes;
-              for (let ct of data[pos].cost) {
-                this.monthCosts += ct.value;
-              }
-            }
-          }
-        }
-        console.log(this.calendarSq);
+      this.date = new Subject<Date>();
+      this.dateSub = this.date.subscribe((date) => {
+        this.startCalendar(date);
+        console.log(date);
+        this.gatherDatafromDb(date);
+        console.log(date);
+        this.dateOnScreen = date;
+        this.calendarDate = date.getFullYear().toString() + '/' + this.calendar[date.getMonth()].month;
       });
+      const dat = new Date();
+      this.date.next(new Date(dat.getFullYear(), dat.getMonth()));
     }
   }
 
@@ -90,6 +65,69 @@ export class CalendarComponent implements OnInit {
     const dateToEnter: Date = new Date(date.getFullYear(), date.getMonth(), day);
     this.dataService.setDate(dateToEnter);
     this.router.navigate(['today']);
+  }
+
+  startCalendar(stDate: Date) {
+    this.monthResume = new Day();
+    this.calendarSq = new Array<Array<CalendarSquare>>();
+    for (let x = 0; x < 7; x++) {
+      this.calendarSq.push(new Array<CalendarSquare>());
+    }
+    this.endDay = this.calendar[stDate.getMonth()].days;
+    let st = 1;
+    if (stDate.getMonth() === 1 && stDate.getFullYear() % 4 === 0) {
+      this.endDay++;
+    }
+    st -= stDate.getDay();
+    // console.log(st);
+    for (let x = 0; x < 42; x++) {
+      let sq = new CalendarSquare();
+      const p = Math.floor(x / 7);
+      sq.index = x + st;
+      if (x + st <= 0 || x + st > this.endDay) {
+        sq.active = -1;
+      } else {
+        sq.active = 0;
+      }
+      this.calendarSq[p].push(sq);
+    }
+    console.log(this.calendar[stDate.getMonth()].month);
+  }
+
+  gatherDatafromDb(stDate: Date) {
+    this.db.getMonthInfo(stDate, this.endDay).subscribe((data: Array<Day>) => {
+      for (let sqArray of this.calendarSq) {
+        for (let sq of sqArray) {
+          const pos = data.findIndex((day) => day.created.getDate() === sq.index);
+          if ( pos !== -1) {
+            sq.active = 1;
+            this.monthResume.gains += data[pos].gains;
+            this.monthResume.type1Clothes += data[pos].type1Clothes;
+            this.monthResume.type2Clothes += data[pos].type2Clothes;
+            for (let ct of data[pos].cost) {
+              this.monthCosts += ct.value;
+            }
+          }
+        }
+      }
+      console.log(this.calendarSq);
+    });
+  }
+
+  moveDate(month: number) {
+    console.log(month);
+    console.log(this.dateOnScreen.getMonth());
+    let year = this.dateOnScreen.getFullYear();
+    month += this.dateOnScreen.getMonth();
+    if (month > 11) {
+      month = 0;
+      year++;
+    } else if (month < 0) {
+      month = 11;
+      year--;
+    }
+    console.log(month);
+    this.date.next(new Date(year, month));
   }
 
 }
