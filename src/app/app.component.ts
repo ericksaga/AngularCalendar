d41783efc26bd087ba6df01122dca032c55463ab
ericksaga <erickmedina09@gmail.com>
2020-07-26 23:05:25 -0400

    added navBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataManagementService } from './services/dataManagement/data-management.service';
+import { User } from './classes/user';
+import { Subscription } from 'rxjs';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'AngularCalendar';
+  user: User;
+  userSub: Subscription;
+  constructor(private dataService: DataManagementService,
+              private auth: AuthService) {  }
+
+  ngOnInit(): void {
+    this.userSub = this.dataService.getUserObservable().subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+
+  logout(): void {
+    this.auth.logout();
+    this.dataService.setUser(undefined);
+  }
 }
